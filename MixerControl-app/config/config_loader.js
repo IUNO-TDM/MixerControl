1d63186c0b10abb76f80f3189e8c6833d7b4dd5f
Manuel Beuttler <manuel.beuttler@de.trumpf.com>
2017-06-07 12:47:16 +0200

    Fixed env variable name

--- MixerControl-app/config/config_loader.js
+++ MixerControl-app/config/config_loader.js
@@ -7,7 +7,9 @@
  * To use a custom config file use the config_default as a template.
  * Save the custom config file in the same directory. All files starting with private* will be ignored by git.
  *
- * Set the file name as environment variable for TDM_CORE_CONFIG.
+ * Set the file name as environment variable for TDM_MIXER_CONFIG.
+ *
+ * Example: TDM_MIXER_CONFIG='private_config_local' node bin/www
  *
  * If the custom config is missing any variables, the default variables will be used.
  * If no custom config is defined, the default configuration will be loaded.
@@ -15,7 +17,7 @@
  * @returns {config}
  */
 
-var ENV_NAME = 'MC_CONFIG';
+var ENV_NAME = 'TDM_MIXER_CONFIG';
 var config = require('./config_defaults');
 
 if (process.env[ENV_NAME]) {
