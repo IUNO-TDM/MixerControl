d780a9bc10c3d978c4afc2b88d0e4e873112832d
Manuel Beuttler <manuel.beuttler@de.trumpf.com>
2017-07-18 17:16:34 +0200

    Updated config

---
+++ MixerControl-app/config/config_test.js
@@ -0,0 +1,57 @@
+/**
+ * Created by beuttlerma on 18.04.17.
+ */
+
+
+var self = {};
+
+
+// ---- CONFIGURATION EXPORT ----
+
+self.DATABASE_NAME = 'MIXERCONTROL_DB';
+self.LOG_LEVEL = 'debug';
+self.HOST_SETTINGS = {
+    JUICE_MACHINE_SERVICE: {
+        HOST: 'test-tdm.fritz.box',
+        PORT: 3001,
+        PROTOCOL: 'http'
+    },
+    OAUTH_SERVER: {
+        HOST: 'test-tdm.fritz.box',
+        PORT: 3005,
+        PROTOCOL: 'https'
+    }
+};
+
+// ---- INGREDIENT CONFIGURATION ----
+self.STD_INGREDIENT_CONFIGURATION = [
+    "570a5df0-a044-4e22-b6e6-b10af872d75c",
+    "198f1571-4846-4467-967a-00427ab0208d",
+    "f6d361a9-5a6f-42ad-bff7-0913750809e4",
+    "fac1ee6f-185f-47fb-8c56-af57cd428aa8",
+    "0425393d-5b84-4815-8eda-1c27d35766cf",
+    "4cfa2890-6abd-4e21-a7ab-17613ed9a5c9",
+    "14b72ce5-fec1-48ec-83ff-24b124f98dc8",
+    "bf2cfd66-5b6f-4655-8e7f-04090308f6db"
+];
+
+self.STD_INGREDIENT_AMOUNT = [
+    1500,
+    1500,
+    1500,
+    1000,
+    1000,
+    1000,
+    1000,
+    1000
+];
+
+self.OAUTH_CREDENTIALS = {
+    CLIENT_ID: 'c28df0ac-7cab-40e4-ba78-4f9b49465767',
+    CLIENT_SECRET: '7b139abc-a176-478e-b1b0-aaf371f7ebea'
+};
+
+self.USER_UUID = '168955dc-d057-4826-99ce-732c60c8f91e';
+
+
+module.exports = self;
