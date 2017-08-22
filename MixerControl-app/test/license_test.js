3609a3a21a18b86efdcddb140c139d6c63419c3c
Manuel Beuttler <manuel.beuttler@de.trumpf.com>
2017-08-22 17:14:58 +0200

    some license tests

---
+++ MixerControl-app/test/license_test.js
@@ -0,0 +1,21 @@
+
+const fs = require('fs');
+const path = require('path');
+const jms  = require('../adapter/juice_machine_service_adapter');
+
+
+const cmDongleId = '3-4019155';
+const contextFilePath = path.resolve(__dirname, cmDongleId + '.WibuCmRaC');
+const updateFilePath = path.resolve(__dirname, cmDongleId + '.WibuCmRaU')
+const context = new Buffer(fs.readFileSync(contextFilePath)).toString('base64');
+
+
+jms.getLicenseUpdate(cmDongleId, context, function(err, update) {
+    if (err) {
+        console.error(err);
+    }
+
+    if (update) {
+        fs.writeFileSync(updateFilePath, update, 'base64');
+    }
+});
