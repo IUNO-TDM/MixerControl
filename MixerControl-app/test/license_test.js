ee9712dd7d223edeb0e76218aa298741a76875c6
Manuel Beuttler <manuel.beuttler@de.trumpf.com>
2017-08-31 10:47:54 +0200

    updated license test and ignore file

--- MixerControl-app/test/license_test.js
+++ MixerControl-app/test/license_test.js
@@ -4,9 +4,9 @@ const path = require('path');
 const jms  = require('../adapter/juice_machine_service_adapter');
 
 
-const cmDongleId = '3-4019155';
+const cmDongleId = '3-4019154';
 const contextFilePath = path.resolve(__dirname, cmDongleId + '.WibuCmRaC');
-const updateFilePath = path.resolve(__dirname, cmDongleId + '.WibuCmRaU')
+const updateFilePath = path.resolve(__dirname, cmDongleId + '.WibuCmRaU');
 const context = new Buffer(fs.readFileSync(contextFilePath)).toString('base64');
 
 
