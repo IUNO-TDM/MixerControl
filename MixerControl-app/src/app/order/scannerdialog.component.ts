bf8cc4ca288128d4167ab50e34e79b6a7ed73dba
Christian Görg <christian.goerg@de.trumpf.com>
2017-09-13 14:34:47 +0200

    fixed details in scanner dialog

--- MixerControl-app/src/app/order/scannerdialog.component.ts
+++ MixerControl-app/src/app/order/scannerdialog.component.ts
@@ -4,6 +4,7 @@ import {OrderService} from "../services/order.service";
 import {QrScannerComponent} from 'angular2-qrscanner';
 import {DrinkService} from '../services/drink.service'
 import {Drink} from "../models/Drink";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'scan-dialog',
@@ -45,7 +46,8 @@ export class ScanDialog implements OnInit{
     @Inject(MD_DIALOG_DATA) public data: any,
     private orderService: OrderService,
     private drinkService: DrinkService,
-    public snackBar: MdSnackBar) {}
+    public snackBar: MdSnackBar,
+    private router: Router) {}
 
   ngOnInit(){
 
@@ -60,23 +62,24 @@ export class ScanDialog implements OnInit{
     console.log("Scanned QR-Code: " + text);
     this.orderService.sendPayment(this.data.order.orderNumber, text)
       .then(response => {
-          console.log(response);
-          if (response.status != 200) {
-            console.log("This should not happen. I cannot believe that this codepath is active");
-            this.qrScannerComponent.startScanning();
-          }else {
-            // this.dialogRef.close();
-          }
           var data = response.json();
-          if(this.drink && data && data.coin){
+          console.log(this.drink);
+          console.log(data);
+          if(this.drink){
+            console.log("dfghjkl");
             if(data.coin < this.drink.retailPrice) {
               let config = new MdSnackBarConfig();
-              this.snackBar.open("Der Coupon hat mit " + data.coin / 100000 + " IUNO zu wenig Guthaben", "Ok", {duration: 5000});
+              this.snackBar.open("Der Coupon hat mit " + data.coin / 100000 + " IUNO zu wenig Guthaben", "", {duration: 5000});
+              this.qrScannerComponent.startScanning()
             }else{
               let config = new MdSnackBarConfig();
               this.snackBar.open("Auf dem Coupon verbleibt ein Guthaben von " + (data.coin - this.drink.retailPrice)/ 100000 + " IUNO", "", {duration: 5000});
+              this.dialogRef.close();
             }
+          }else {
+            this.dialogRef.close();
           }
+
 
         },
         resp => {
@@ -84,21 +87,22 @@ export class ScanDialog implements OnInit{
           if(resp.status){
             if(resp.status == 404){
               let config = new MdSnackBarConfig();
-              this.snackBar.open("Der Zahlungsauftrag ist im PaymentService nicht vorhanden","Neuer Auftrag",{duration: 5000});
+              let snackBarRef = this.snackBar.open("Der Zahlungsauftrag ist im PaymentService nicht vorhanden","Neuer Auftrag",{duration: 5000});
+              snackBarRef.onAction().subscribe(()=> this.router.navigateByUrl('/drink/'+this.drink.id));
             }else if(resp.status == 422){
               let config = new MdSnackBarConfig();
               this.snackBar.open("Ungültiger Coupon","OK",{duration: 5000});
               this.qrScannerComponent.startScanning();
             } else if(resp.status == 409){
               let config = new MdSnackBarConfig();
-              this.snackBar.open("Dieser Coupon wurde bereits für diesen Auftrag eingescannt.","OK",{duration: 5000});
+              this.snackBar.open("Dieser Coupon wurde bereits für diesen Auftrag eingescannt.","",{duration: 5000});
               this.qrScannerComponent.startScanning();
             }else{
-              this.snackBar.open(resp,"OK",{duration: 5000});
+              this.snackBar.open(resp,"",{duration: 5000});
               this.qrScannerComponent.startScanning();
             }
           }else{
-            this.snackBar.open(resp,"OK",{duration: 5000});
+            this.snackBar.open(resp,"",{duration: 5000});
             this.qrScannerComponent.startScanning();
           }
           // console.log(err);
