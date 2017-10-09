7fa47d1266172c4b35e094883026501bb75eb17a
Manuel Beuttler <manuel.beuttler@de.trumpf.com>
2017-10-09 14:38:06 +0200

    better error handling in license_manager_strategy

--- MixerControl-app/adapter/license_manager_adapter.js
+++ MixerControl-app/adapter/license_manager_adapter.js
@@ -1,12 +1,10 @@
-
-
-let self = require('./license_manager_strategy/default');
+let self = {};
 
 try {
     self = require('./license_manager_strategy/' + process.env.NODE_ENV);
 }
 catch (err) {
-
+    self = require('./license_manager_strategy/default');
 }
 
 module.exports = self;
