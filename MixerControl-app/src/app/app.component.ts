df7269dbbf51f344ff1d33939c2ee87f59512dce
Christian Görg <christian.goerg@de.trumpf.com>
2017-10-16 14:26:01 +0200

    client side internet connection tracking closes #57

--- MixerControl-app/src/app/app.component.ts
+++ MixerControl-app/src/app/app.component.ts
@@ -8,15 +8,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MixerControl';
-  // checked = false;
-  // indeterminate = false;
-  // align = 'start';
-  // disabled = false;
-  //
-  // tiles = [
-  //   {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-  //   {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-  //   {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-  //   {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
-  // ];
 }
