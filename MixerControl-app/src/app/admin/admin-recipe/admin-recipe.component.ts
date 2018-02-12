37c56cbd71733824d1bf8179133d8f28b5d4a832
Manuel Beuttler <manuel.beuttler@de.trumpf.com>
2018-02-12 10:22:11 +0100

    UI optimzations for creating recipes
    Former-commit-id: b4b950fe4fe71ded11969bc7868478a5c06ae89e

--- MixerControl-app/src/app/admin/admin-recipe/admin-recipe.component.ts
+++ MixerControl-app/src/app/admin/admin-recipe/admin-recipe.component.ts
@@ -3,30 +3,36 @@ import {Component as TdmComponent} from '../../models/Component';
 import {TdmProgram} from '../../juice-program-configurator/models/tdmprogram';
 import {ComponentService} from "../../services/component.service";
 import {AdminService} from "../../services/admin.service";
+import {Subscription} from "rxjs/Rx";
+import {ProductionSocketService} from "../../services/production-socket.service";
 
 @Component({
     selector: 'app-admin-recipe',
     templateUrl: './admin-recipe.component.html',
     styleUrls: ['./admin-recipe.component.css'],
-    providers: [ComponentService, AdminService]
+    providers: [ComponentService, AdminService, ProductionSocketService]
 })
 export class AdminRecipeComponent implements OnInit {
 
-
+    pcModeConnection: Subscription;
     components: TdmComponent[];
     program = new TdmProgram();
-
+    pcMode: string;
 
     constructor(
         private componentService: ComponentService,
-        private adminService: AdminService
+        private adminService: AdminService,
+        private productionSocketService: ProductionSocketService
         ) {}
 
     ngOnInit() {
 
         this.componentService.getComponents(false).then(components => this.components = components);
 
-
+        this.pcModeConnection = this.productionSocketService.getUpdates('pumpControlMode')
+            .subscribe(state => {
+                this.pcMode = state;
+            });
     }
 
     onProduce() {
