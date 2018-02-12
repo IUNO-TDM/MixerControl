2c62ae170f617f22659d6a54fb44816071629ac2
Manuel Beuttler <manuel.beuttler@de.trumpf.com>
2018-02-12 08:51:54 +0100

    disabling animations
    Former-commit-id: 0aed0d3c71f7a3a4f5d60e75734b339fc1840add

--- MixerControl-app/src/app/recipe-overview/recipe-overview.module.ts
+++ MixerControl-app/src/app/recipe-overview/recipe-overview.module.ts
@@ -2,9 +2,11 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RecipeOverviewComponent} from './recipe-overview/recipe-overview.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+
 import {
-  MatButtonModule, MatButtonToggleModule, MatCardModule, MatChipsModule, MatDialogModule,
-  MatSidenavModule, MatIconModule, MatTooltipModule
+    MatButtonModule, MatButtonToggleModule, MatCardModule, MatChipsModule, MatDialogModule,
+    MatSidenavModule, MatIconModule, MatTooltipModule
 } from '@angular/material';
 import 'hammerjs';
 import {FlexLayoutModule} from '@angular/flex-layout';
@@ -13,23 +15,24 @@ import {RecipeFilterPipe} from './recipe-overview/recipe-filter-pipe';
 import {FormsModule} from '@angular/forms';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    MatCardModule,
-    MatDialogModule,
-    MatChipsModule,
-    MatButtonModule,
-    FlexLayoutModule,
-    MatSidenavModule,
-    MatButtonToggleModule,
-    MatIconModule,
-    MatTooltipModule
-  ],
-  declarations: [RecipeOverviewComponent, RecipeDetailComponent, RecipeFilterPipe],
-  exports: [RecipeOverviewComponent],
-  entryComponents: [RecipeDetailComponent]
+    imports: [
+        CommonModule,
+        // BrowserAnimationsModule,
+        NoopAnimationsModule,
+        FormsModule,
+        MatCardModule,
+        MatDialogModule,
+        MatChipsModule,
+        MatButtonModule,
+        FlexLayoutModule,
+        MatSidenavModule,
+        MatButtonToggleModule,
+        MatIconModule,
+        MatTooltipModule
+    ],
+    declarations: [RecipeOverviewComponent, RecipeDetailComponent, RecipeFilterPipe],
+    exports: [RecipeOverviewComponent],
+    entryComponents: [RecipeDetailComponent]
 })
 export class RecipeOverviewModule {
 }
