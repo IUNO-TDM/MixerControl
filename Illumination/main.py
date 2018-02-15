1944b998721e0dd21d09740c0356d3769b6867ee
Hans-Peter Bock <Hans-Peter.Bock@de.trumpf.com>
2018-02-15 16:16:04 +0100

    outsourced dotstar class to separate file

--- Illumination/main.py
+++ Illumination/main.py
@@ -4,10 +4,9 @@ import threading
 import time
 import sys
 import os
-
 import argparse
 
-import spidev
+import DotStar
 
 from socketIO_client import SocketIO, BaseNamespace
 
@@ -101,45 +100,6 @@ class socketIoThread (threading.Thread):
             print "Exiting " + self.name
 
 #####
-# DotStar SPI class
-###
-class dotStars():
-    def __init__(self):
-        # Calculate gamma correction table, makes mid-range colors look 'right':
-        self.gamma = bytearray(256)
-        for i in range(256):
-	        self.gamma[i] = int(pow(float(i) / 255.0, 2.7) * 255.0 + 0.5)
-
-        self.spi = spidev.SpiDev()
-
-        self.spiArray = [0b11100000 | (maxBrightness & 31)] * (4 + numPixels * 4 + 1)
-
-        # start frame 0x00000000
-        self.spiArray[0] = 0
-        self.spiArray[1] = 0
-        self.spiArray[2] = 0
-        self.spiArray[3] = 0
-
-        # end frame
-        self.spiArray[(4 + numPixels * 4)] = 0xff
-
-    def open(self, spidev, spics):
-        self.spi.open(spidev, spics)
-        self.spi.max_speed_hz = 1600000
-
-    def setPixel(self, index, r, g, b):
-#        self.spiArray[4 + 4 * index + 0] = 0b11100000 | (maxBrightness & 31)
-        self.spiArray[4 + 4 * index + 1] = self.gamma[b] # blue
-        self.spiArray[4 + 4 * index + 2] = self.gamma[g] # green
-        self.spiArray[4 + 4 * index + 3] = self.gamma[r] # red
-
-    def show(self):
-        self.spi.writebytes(self.spiArray)
-
-    def close(self):
-        self.spi.close()
-
-#####
 # Dot Stars Animator
 ###
 HZ = 50
@@ -182,7 +142,7 @@ class dotStarsThread (threading.Thread):
 
     def run(self):
         print "Starting " + self.name
-        ds = dotStars()
+        ds = DotStar.DotStar(numPixels, maxBrightness)
         ds.open(args.spidev, args.spics)
         
         while not endThreads:
