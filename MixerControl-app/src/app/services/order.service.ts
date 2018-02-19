a19962ca7e245bdbf6570db94629d26f5ea81af3
Manuel Beuttler <manuel.beuttler@de.trumpf.com>
2018-02-19 08:10:50 +0100

    Reformatting

--- MixerControl-app/src/app/services/order.service.ts
+++ MixerControl-app/src/app/services/order.service.ts
@@ -1,44 +1,44 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 import 'rxjs/add/operator/map';
-import { Observable } from 'rxjs/Rx';
+import {Observable} from 'rxjs/Rx';
 
 import {Order} from '../models/Order';
 import {AdressValuePair} from '../models/AdressValuePair';
 
 import {HttpClient, HttpResponse} from '@angular/common/http';
+
 @Injectable()
 export class OrderService {
-  private ordersUrl = 'api/orders/';  // URL to web api
+    private ordersUrl = 'api/orders/';  // URL to web api
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) {
+    }
 
-  getOrder(id: string): Observable<Order> {
-    const url = `${this.ordersUrl}/${id}`;
-    return this.http.get<Order>(url);
-  }
+    getOrder(id: string): Observable<Order> {
+        const url = `${this.ordersUrl}/${id}`;
+        return this.http.get<Order>(url);
+    }
 
 
-  createOrder(order: Order): Observable<Order> {
-    return this.http.post<Order>(this.ordersUrl, order);
-  }
+    createOrder(order: Order): Observable<Order> {
+        return this.http.post<Order>(this.ordersUrl, order);
+    }
 
-  getPaymentRequest(id: string): Observable<string> {
-    const url = `${this.ordersUrl}${id}/PaymentRequest`;
-    return this.http.get(url).map(resp => resp['PaymentRequest']);
-  }
+    getPaymentRequest(id: string): Observable<string> {
+        const url = `${this.ordersUrl}${id}/PaymentRequest`;
+        return this.http.get(url).map(resp => resp['PaymentRequest']);
+    }
 
-  sendPayment(id: string, payment: string): Observable<AdressValuePair> {
-    const url = `${this.ordersUrl}${id}/Payment`;
+    sendPayment(id: string, payment: string): Observable<AdressValuePair> {
+        const url = `${this.ordersUrl}${id}/Payment`;
 
-    return this.http.put<AdressValuePair>(url, payment);
-  }
+        return this.http.put<AdressValuePair>(url, payment);
+    }
 
-  sendProductionStart(id: string): Observable<HttpResponse<Object>> {
-    // const headers = new Headers({ 'Content-Type': 'text/plain' });
-    // const options = new RequestOptions({ headers: headers });
-    const url = `${this.ordersUrl}${id}/productionStart`;
+    sendProductionStart(id: string): Observable<HttpResponse<Object>> {
+        const url = `${this.ordersUrl}${id}/productionStart`;
 
-    return this.http.put(url, 'true', {observe: 'response'});
-  }
+        return this.http.put(url, 'true', {observe: 'response'});
+    }
 }
